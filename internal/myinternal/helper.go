7dc5d551c100cb16df6dd8f8d4d64b95e200123a
Mamorsky <igor.mamorsky@appsflyer.com>
2024-09-21 12:28:15 +0300

    Added basic structure

---
+++ internal/myinternal/helper.go
@@ -0,0 +1,7 @@
+package myinternal
+
+import "fmt"
+
+func DoSomethingInternal() {
+	fmt.Println("This is an internal helper function!")
+}
---
+++ pkg/somepackage/somefile.go
@@ -0,0 +1,7 @@
+package somepackage
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello from somepackage!")
+}
