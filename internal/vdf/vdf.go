4ef21e34799938b7b8c8f8d113e0dcfe24f4356f
Igor Mamorski <igor.mamorski@gmail.com>
2024-10-26 13:47:33 +0300

    Added VDF mock with tests

---
+++ internal/vdf/vdf.go
@@ -0,0 +1,64 @@
+package vdf
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"time"
+)
+
+type VDF interface {
+	Setup(lambda, delta int) ([]byte, error)
+	Eval(x, vk []byte, delta int) ([]byte, []byte, error)
+	Verify(x, phi, pi, vk []byte) (bool, error)
+}
+
+type vdf struct {
+	lambda int
+	delta  int
+	vk     []byte
+}
+
+func New() VDF {
+	return &vdf{}
+}
+
+func (v *vdf) Setup(lambda, delta int) ([]byte, error) {
+	v.lambda = lambda
+	v.delta = delta
+
+	vk := make([]byte, lambda/8)
+	_, err := rand.Read(vk)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate verification key: %w", err)
+	}
+
+	v.vk = vk
+	return vk, nil
+}
+
+func (v *vdf) Eval(x, vk []byte, delta int) ([]byte, []byte, error) {
+	time.Sleep(time.Duration(delta) * time.Second)
+	hash := sha256.Sum256(append(x, vk...))
+	phi := hash[:]
+
+	proof := sha256.Sum256(phi)
+
+	return phi, proof[:], nil
+}
+
+func (v *vdf) Verify(x, phi, pi, vk []byte) (bool, error) {
+	hash := sha256.Sum256(append(x, vk...))
+	expectedPhi := hash[:]
+
+	if string(phi) != string(expectedPhi) {
+		return false, nil
+	}
+
+	proof := sha256.Sum256(phi)
+	if string(proof[:]) != string(pi) {
+		return false, nil
+	}
+
+	return true, nil
+}
