544f4427d14062bbee112288b4f4086b3f7e4edc
Mamorsky <igor.mamorsky@appsflyer.com>
2025-05-28 21:33:16 +0300

    Added mock network for full integration tests

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -1,1 +1,15 @@
 package tests
+
+import (
+	"testing"
+)
+
+// TestNetworkInterface verifies that MockNetwork implements the Network interface
+func TestNetworkInterface(t *testing.T) {
+	cluster := NewMockNetworkCluster(20)
+	err := cluster.ConnectRandom(4)
+	if err != nil {
+		t.Fatalf("Failed to connect random peers: %v", err)
+	}
+
+}
